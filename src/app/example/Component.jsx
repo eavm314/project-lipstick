d5614514b5385c7ebab151690c5228f6407bad7a
Vivian Marino <marino6733784@gmail.com>
2023-08-12 14:46:59 -0400

    creacion de estilos para botones, titulos, subtitulos, texto normal y texto de preciosademas de la instanciacion de la paleta de colores en root, al igual que tamaños de texto y fuentes

--- src/app/example/Component.jsx
+++ src/app/example/Component.jsx
@@ -1,4 +1,4 @@
-import "./fonts.css"
+import "../globals.css"
 
 export const Component = ({ n, nombre }) => {
   return (
--- src/app/example/page.jsx
+++ src/app/example/page.jsx
@@ -1,25 +1,44 @@
 "use client"
 import { useState } from "react";
 import { Component } from "./Component";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMagnifyingGlass,
+  faCircleStop
+} from "@fortawesome/free-solid-svg-icons";
+import '../globals.css'
+
 
 const ExamplePage = () => {
     const [cont, setCont] = useState(2);
     // let cont = 2;
-    const nums = [1,"dew",21];
+    const nums = [1,2,3,4,5,6];
     const data = {
         nombre: "producto 1",
         precio: 21,
     }
+    const em = 16;
     return(
         <div className="bg-emerald-800 md:bg-yellow-500">
-            <h1>Example</h1>
-            { nums.filter((num)=> num%2==0).map((num) => <Component n={num} nombre={data.nombre}/>)}
+            
+            <h1 className="texto-precio-producto-pagina-individual">ExampleHola123456789</h1>
+            
+            {nums.filter((num)=> num%2==0).map((num) => <Component n={num} nombre={data.nombre}/>)}
             <button
+                style={{fontSize: (em*1)}}
+                className="boton-advertencia"
                 onClick={()=>{setCont(cont+1)}}
                 // onClick={()=>{cont++}}
             >
-                Boton {cont}
+                BotonHola {cont}
             </button>
+                <FontAwesomeIcon
+                    icon={faMagnifyingGlass}
+                    style={{color: "#d6d6d6",}}
+                />
+                <FontAwesomeIcon
+                    icon={faCircleStop}
+                />
 
         </div>
     )
