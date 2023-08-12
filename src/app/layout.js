c77f0d35e2d859db58a723083ce97bdab22287bc
Vivian Marino <marino6733784@gmail.com>
2023-08-12 00:25:51 -0400

    implementacion font awesome icons

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,12 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 
+//implementar font awesome icons
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+
+config.autoAddCss = false;
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
