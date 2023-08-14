c25724cc36c49bc8567f4ced377835a460d6ed4f
Vivian Marino <marino6733784@gmail.com>
2023-08-14 17:20:30 -0400

    layouts temporales, creacion de componentes para tienda, version preliminar de tienda, ajuste de configuraciones en globals.css

---
+++ src/app/tienda/ComponentesTienda.jsx
@@ -0,0 +1,36 @@
+import "../globals.css"
+
+const em = 16;
+
+export const BotonProducto = ({ imagen, categoria, tags, nombre, precio, textoBoton }) => {
+  return (
+    <button className="boton-producto">
+            <div className="imagen-producto-tienda">
+                <img src={"/"+imagen+".jpg"} alt=""/>
+            </div>
+
+            <p className="texto-normal-semibold"
+            style={{fontSize: (em*1), width: (9.75*em)}}>
+                {nombre}
+            </p>
+
+            <p className="texto-tags-producto-tienda"
+            style={{fontSize: (em*0.75), width: (9.75*em)}}>
+                {categoria}: {tags.join(", ")}
+            </p>
+
+            <p className="texto-precio-producto-tienda"
+            style={{fontSize: (em*1.5), width: (9.75*em)}}>
+                ${precio}
+            </p>
+
+            <button
+                style={{fontSize: (em*1)}}
+                className="boton-primario"
+                //TODO: llevar a producto individual
+            >
+                {textoBoton} 
+            </button>
+    </button>
+  )
+}
---
+++ src/app/tienda/page.jsx
@@ -0,0 +1,36 @@
+"use client"
+import { useState } from "react";
+import Image from 'next/image'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMagnifyingGlass,
+  faCircleStop
+} from "@fortawesome/free-solid-svg-icons";
+import '../globals.css'
+import { BotonProducto } from "./ComponentesTienda";
+
+const dataPrueba = {
+    imagen: "lipstickPrueba",
+    categoria: "Labios",
+    tags: ["lipstick","especial"],
+    nombre: "Ruby Lips",
+    precio: "70.00", 
+    textoBoton: "Comprar"
+}
+
+const TiendaPage = () => {
+    const [cont, setCont] = useState(2);
+    const nums = [1,2,3,4,5,6,7,8];
+    const em = 16;
+    return(
+        <div style={{display: "flex", alignItems: "center", alignContent: "center", padding: (4.25*em), justifyContent: "center"}}>
+            <div className="conjunto-productos">
+                {nums.map((num) => <BotonProducto imagen={dataPrueba.imagen} categoria={dataPrueba.categoria} 
+                tags={dataPrueba.tags} nombre={dataPrueba.nombre} precio={dataPrueba.precio} textoBoton={dataPrueba.textoBoton}/>)}
+            </div>
+        </div>
+        
+    )
+}
+
+export default TiendaPage;
