be1fa51822ef6136f9cd7b9398a2b11403103795
enrique <enriqueadhemar@gmail.com>
2023-08-15 14:19:10 -0400

    fixes

--- src/app/tienda/ComponentesTienda.jsx
+++ src/app/tienda/ComponentesTienda.jsx
@@ -4,33 +4,33 @@ const em = 16;
 
 export const BotonProducto = ({ imagen, categoria, tags, nombre, precio, textoBoton }) => {
   return (
-    <button className="boton-producto">
-            <div className="imagen-producto-tienda">
-                <img src={"/"+imagen+".jpg"} alt=""/>
-            </div>
+    <div className="boton-producto">
+      <div className="imagen-producto-tienda">
+        <img src={"/" + imagen + ".jpg"} alt="" />
+      </div>
 
-            <p className="texto-normal-semibold"
-            style={{fontSize: (em*1), width: (9.75*em)}}>
-                {nombre}
-            </p>
+      <p className="texto-normal-semibold"
+        style={{ fontSize: (em * 1), width: (9.75 * em) }}>
+        {nombre}
+      </p>
 
-            <p className="texto-tags-producto-tienda"
-            style={{fontSize: (em*0.75), width: (9.75*em)}}>
-                {categoria}: {tags.join(", ")}
-            </p>
+      <p className="texto-tags-producto-tienda"
+        style={{ fontSize: (em * 0.75), width: (9.75 * em) }}>
+        {categoria}: {tags.join(", ")}
+      </p>
 
-            <p className="texto-precio-producto-tienda"
-            style={{fontSize: (em*1.5), width: (9.75*em)}}>
-                ${precio}
-            </p>
+      <p className="texto-precio-producto-tienda"
+        style={{ fontSize: (em * 1.5), width: (9.75 * em) }}>
+        ${precio}
+      </p>
 
-            <button
-                style={{fontSize: (em*1)}}
-                className="boton-primario"
-                //TODO: llevar a producto individual
-            >
-                {textoBoton} 
-            </button>
-    </button>
+      <button
+        style={{ fontSize: (em * 1) }}
+        className="boton-primario"
+      //TODO: llevar a producto individual
+      >
+        {textoBoton}
+      </button>
+    </div>
   )
 }
