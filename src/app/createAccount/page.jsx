9f9628876c6cb63974024942114d5d6cfab289b9
dylanJitton <dylanjitton2.0@gmail.com>
2023-08-25 22:27:15 -0400

    correccion US1

--- src/app/createAccount/page.jsx
+++ src/app/createAccount/page.jsx
@@ -5,51 +5,51 @@ const CreateAccount = () => {
     return (
         <div style={{ display: "flex", backgroundColor: "#fff", flexDirection: 'column', justifyContent: 'center', alignItems: 'center', alignContent: 'center' }}>
             <div style={{ backgroundColor: '#efecea', padding: 36, borderWidth: 1, borderColor: '#000' }}>
-                <div style={{ width: 30.25 * 16, height: 36.75 * 16 }}>
-                    <h1 className="titulo" style={{ fontSize: 2 * 16, marginBottom: 35 }}>Regístrate</h1>
-                    <div style={{ display: 'flex', flexDirection: 'row', marginBottom: 16 * 1.25, justifyContent: 'space-between' }}>
-                        <input type='text' style={{ display: 'flex', fontSize: 18, borderWidth: 1, borderColor: '#000', backgroundColor: '#f9f8f7', width: 14.5 * 16, height: 55, padding: 0.75 * 16 }}
+                <div style={{ width: 30.25 * em, height: 36.75 * em }}>
+                    <h1 className="titulo" style={{ fontSize: 2 * em, marginBottom: em*2.1875 }}>Regístrate</h1>
+                    <div style={{ display: 'flex', flexDirection: 'row', marginBottom: em * 1.25, justifyContent: 'space-between' }}>
+                        <input type='text' style={{ display: 'flex', fontSize: 18, borderWidth: 1, borderColor: '#000', backgroundColor: '#f9f8f7', width: 14.5 * em, height: 55, padding: 0.75 * em }}
                             placeholder="Nombre"
                             name="Nombre"
                             className="texto-parrafo" />
-                        <input type='text' style={{ display: 'flex', fontSize: 18, borderWidth: 1, borderColor: '#000', backgroundColor: '#f9f8f7', width: 14.5 * 16, height: 55, padding: 0.75 * 16 }}
+                        <input type='text' style={{ display: 'flex', fontSize: 18, borderWidth: 1, borderColor: '#000', backgroundColor: '#f9f8f7', width: 14.5 * em, height: 55, padding: 0.75 * em }}
                             placeholder="Apellido"
                             name="Apellido"
                             className="texto-parrafo" />
                     </div>
-                    <input type='text' style={{ display: 'flex', fontSize: 18, borderWidth: 1, borderColor: '#000', backgroundColor: '#f9f8f7', width: '100%', height: 55, padding: 0.75 * 16, marginBottom: 16 * 1.25 }}
+                    <input type='text' style={{ display: 'flex', fontSize: 18, borderWidth: 1, borderColor: '#000', backgroundColor: '#f9f8f7', width: '100%', height: 55, padding: 0.75 * em, marginBottom: em * 1.25 }}
                         placeholder="Correo electrónico"
                         name="correo electronico"
                         className="texto-parrafo" />
-                    <h1 className="texto-normal-semibold" style={{ fontSize: 16 * 1.25, marginBottom: 16 * 0.6875 }}>Fecha de Nacimiento</h1>
-                    <div style={{ display: 'flex', flexDirection: 'row', marginBottom: 16 * 1.25, justifyContent: 'flex-start' }}>
-                        <input type='text' style={{ display: 'flex', fontSize: 18, borderWidth: 1, borderColor: '#000', backgroundColor: '#f9f8f7', width: 5.75 * 16, height: 55, padding: 0.75 * 16, marginRight: 2 * 16 }}
+                    <h1 className="texto-normal-semibold" style={{ fontSize: em * 1.25, marginBottom: em * 0.6875 }}>Fecha de Nacimiento</h1>
+                    <div style={{ display: 'flex', flexDirection: 'row', marginBottom: em * 1.25, justifyContent: 'flex-start' }}>
+                        <input type='text' style={{ display: 'flex', fontSize: 18, borderWidth: 1, borderColor: '#000', backgroundColor: '#f9f8f7', width: 5.75 * em, height: 55, padding: 0.75 * em, marginRight: 2 * em }}
                             placeholder="Dia"
                             name="Dia"
                             className="texto-parrafo" />
-                        <input type='text' style={{ display: 'flex', fontSize: 18, borderWidth: 1, borderColor: '#000', backgroundColor: '#f9f8f7', width: 5.75 * 16, height: 55, padding: 0.75 * 16, marginRight: 2 * 16 }}
+                        <input type='text' style={{ display: 'flex', fontSize: 18, borderWidth: 1, borderColor: '#000', backgroundColor: '#f9f8f7', width: 5.75 * em, height: 55, padding: 0.75 * em, marginRight: 2 * em }}
                             placeholder="Mes"
                             name="Mes"
                             className="texto-parrafo" />
-                        <input type='text' style={{ display: 'flex', fontSize: 18, borderWidth: 1, borderColor: '#000', backgroundColor: '#f9f8f7', width: 5.75 * 16, height: 55, padding: 0.75 * 16, marginRight: 2 * 16 }}
+                        <input type='text' style={{ display: 'flex', fontSize: 18, borderWidth: 1, borderColor: '#000', backgroundColor: '#f9f8f7', width: 5.75 * em, height: 55, padding: 0.75 * em, marginRight: 2 * em }}
                             placeholder="Año"
                             name="Año"
                             className="texto-parrafo" />
                     </div>
-                    <input type='text' style={{ display: 'flex', fontSize: 18, borderWidth: 1, borderColor: '#000', backgroundColor: '#f9f8f7', width: '100%', height: 55, padding: 0.75 * 16, marginBottom: 16 * 1.25 }}
+                    <input type='text' style={{ display: 'flex', fontSize: 18, borderWidth: 1, borderColor: '#000', backgroundColor: '#f9f8f7', width: '100%', height: 55, padding: 0.75 * em, marginBottom: em * 1.25 }}
                         placeholder="Contraseña"
                         name="Contraseña"
                         className="texto-parrafo" />
-                    <input type='text' style={{ display: 'flex', fontSize: 18, borderWidth: 1, borderColor: '#000', backgroundColor: '#f9f8f7', width: '100%', height: 55, padding: 0.75 * 16, marginBottom: 16 * 1.25 }}
+                    <input type='text' style={{ display: 'flex', fontSize: 18, borderWidth: 1, borderColor: '#000', backgroundColor: '#f9f8f7', width: '100%', height: 55, padding: 0.75 * em, marginBottom: em * 1.25 }}
                         placeholder="Repite contraseña"
                         name="Repite contraseña"
                         className="texto-parrafo" />
                     <div style={{ display: 'flex', width: '100%', alignItems: 'center' ,flexDirection:'column'}}>
                         <div style={{justifyContent:'center'}}>
-                            <h1 className="texto-parrafo" style={{fontSize:16*1.125}}>Acepto los 
+                            <h1 className="texto-parrafo" style={{fontSize:em*1.125}}>Acepto los 
                                 <Link 
                                 className="texto-parrafo" 
-                                style={{fontSize:16*1.125, fontWeight:'bold',marginLeft:2}}
+                                style={{fontSize:em*1.125, fontWeight:'bold',marginLeft:2}}
                                 href={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"}> Términos y Condiciones</Link>
                                 
                                 {/*TODO: IMPLEMENTAR SISTEMA DE TICKEADO*/}
