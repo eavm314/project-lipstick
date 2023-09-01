690acc29af525e4afb1b8b18f704da549c597d9a
enrique <enriqueadhemar@gmail.com>
2023-09-01 13:51:54 -0400

    borre lo de perry

--- src/api_services/productsService.js
+++ src/api_services/productsService.js
@@ -1,9 +1,12 @@
-const { listaProductos } = require("@/data/listaProductos")
+import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 
 export const getProducts = async () => {
-    const products = listaProductos;
-    return products;
-}   
+    const supabase = createServerActionClient({cookies});
+    const {data}  = await supabase.from("productos").select('*');
+    console.log("🚀 ~ file: productsService.js:9 ~ getProducts ~ data:", data)
+    return data;
+} 
 
 export const getProductById = async (id) => {
     const product = listaProductos.filter(p => p.id == id)[0];
