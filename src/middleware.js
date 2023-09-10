ab2511d069176555ea1ffe5a383967b651050ea1
enrique <enriqueadhemar@gmail.com>
2023-09-10 09:48:42 -0400

    bloquear ingreso sin login

--- src/middleware.js
+++ src/middleware.js
@@ -1,13 +1,20 @@
 import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { redirect } from 'next/dist/server/api-utils';
 import { NextResponse } from 'next/server'
-
 
 export async function middleware(req) {
   const res = NextResponse.next();
-  
+  if (req.nextUrl.pathname.startsWith('/login')) {
+    return res;
+  }
+
   const supabase = createMiddlewareClient({ req, res });
   const {data} = await supabase.auth.getSession();
-    
-//   console.log(data)
+  console.log(data);
+  if (data.session == null) {
+    // console.log("no hay login");
+    // res.redirect("/login");
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
   return res;
 }
